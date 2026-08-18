caeab416b0569e1ba28d82d5cf01ae192fd3ec1c
Mikael <kaelfei91@gmail.com>
2026-08-18 15:50:54 -0300

    Refactor game logic and add character movement

--- main.py
+++ main.py
@@ -1,86 +1,77 @@
+#survival vampire
 import pyxel
-
-
-class Bola:
-    
-    def __init__(self,vx,vy,cor):
+class Personagem():
+    def __init__(self):
         
-        self.x = 10
+        self.x = 80
         self.y = 60
-        self.vx = vx
-        self.vy = vy
-        self.gravidade = 0.9
-        self.arrasto = 0.1
-        self.cor = cor
+        self.direcao = "esquerda"
         
+    def movimento(self):
+        
+        if pyxel.btn(pyxel.KEY_W):
+            self.y -= 2
+            self.direcao = "cima"
+            
+        if pyxel.btn(pyxel.KEY_S):
+            self.y += 2
+            self.direcao = "baixo"
+            
+        if pyxel.btn(pyxel.KEY_A):
+            self.x -= 2
+            self.direcao = "esquerda"
+            
+        if pyxel.btn(pyxel.KEY_D):
+            self.x += 2
+            self.direcao = "direita"
+            
     def desenhar(self):
-        pyxel.pset(self.x,self.y,self.cor)
+        x = pyxel.mouse_x
+        y = pyxel.mouse_y
         
-    def queda(self):
+        pyxel.rect(self.x,self.y,2,2,7)
+        if pyxel.btn(pyxel.KEY_SPACE):
+            pyxel.line(self.x,self.y,x,y,7)
+        
+        if self.direcao == "cima":
+            pyxel.rect(self.x,self.y-2,2,2,8)
             
-        self.vy += self.gravidade
-        self.y += self.vy
-        if self.vx > 0:
-            self.vx -= self.arrasto
-            if self.vx == 0:
-                self.x = 0
-                
-        if self.vx < 0:
-            self.vx += self.arrasto
-            if self.vx > 0:
-                self.vx = 0
-        self.x += self.vx
-        
-    def quique(self):
-        
-        if self.y >= 119:
-            self.vy = -(self.vy / 1.5)
-        if self.x >= 159:
-            self.vx = -(self.vx/1.5)
-        if self.x <= 0:
-            self.vx = -(self.vx/1.5)
+        if self.direcao == "baixo":
+            pyxel.rect(self.x,self.y+2,2,2,8)
             
-        
+        if self.direcao == "direita":
+            pyxel.rect(self.x+2,self.y,2,2,8)
+            
+        if self.direcao == "esquerda":
+            pyxel.rect(self.x-2,self.y,2,2,8)
+            
     def colicao(self):
+        
         if self.x <= 0:
             self.x = 0
-        if self.x >= 159:
-            self.x = 159
+            
+        if self.x >= 158:
+            self.x = 158
+            
+        if self.y >= 118:
+            self.y = 118
+            
         if self.y <= 0:
             self.y = 0
-        if self.y >= 119:
-            self.y = 119
-            
-quantidade = int(input("quantos objetos quer?(máximo 15)"))
-i = 1
-lista = []
-while i <= quantidade:
     
-    a = int(input(f"qual velocidade vx{i}:..."))
-    b = int(input(f"qual velocidade vy{i}:..."))
-    
-    if i <= 15:
-        lista.append(Bola(a,b,i))
-        i += 1
-    elif i <= 0:
-        print("numero inválido...")
-    else:
-        print("numero maximo: 15")
-        
+jogador = Personagem()
+
 def update():
-    for objeto in lista:
-        objeto.queda()
-        objeto.colicao()
-        objeto.quique()
+    
+    jogador.movimento()
+    jogador.colicao()
     
 def draw():
     
-    for objeto in lista:
-        objeto.desenhar()
-        
-pyxel.init(161,121,fps = 30)
-pyxel.run(update,draw)
-
+    pyxel.cls(0)
+    jogador.desenhar()
+    pyxel.text(0,0,f"{jogador.direcao}",7)
     
-        
+pyxel.init(160,120)
+pyxel.run(update,draw)
         
